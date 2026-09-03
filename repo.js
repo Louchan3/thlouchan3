0da36bbe9cc3000b3674318e74c5c19e9c5d31f5
Louchan3 <touhouqualityrips@gmail.com>
2026-09-03 23:18:54 +0300

    Update repo.js

--- repo.js
+++ repo.js
@@ -1,8 +1,6 @@
 {
     "id": "Louchan3",
-    "servers": [
-        
-    ],
+    "servers": ["https://raw.githubusercontent.com/Louchan3/thlouchan3/master/"],
     "title": "Louchan3's High Quality Music Mod",
     "contact": "https://www.youtube.com/@Louchan3",
     "patches": {
