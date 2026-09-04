072bfb060a6f6f7b0477c326b20ffcc2717eb4a1
Louchan3 <touhouqualityrips@gmail.com>
2026-09-04 15:06:00 +0200

    Update repo.js

--- repo.js
+++ repo.js
@@ -1,9 +1,11 @@
 {
-    "id": "thlouchan3",
-    "servers": ["https://raw.githubusercontent.com/Louchan3/thlouchan3/master/"],
-    "title": "Louchan3's High Quality Music Mod",
-    "contact": "https://www.youtube.com/@Louchan3",
-    "patches": {
-        "EOHQD": "Embodiment of High Quality Devil"
-    }
+	"contact": "https://www.youtube.com/@Louchan3",
+	"id": "thlouchan3",
+	"patches": {
+		"EOHQD": "Embodiment of High Quality Devil"
+	},
+	"servers": [
+		"https://raw.githubusercontent.com/Louchan3/thlouchan3/master/"
+	],
+	"title": "Louchan3's High Quality Music Mod"
 }
