87f732baeaad3ee2db6ffa68f409e652907a170a
Louchan3 <touhouqualityrips@gmail.com>
2026-09-04 15:05:42 +0200

    Update patch.js

--- EOHQD/patch.js
+++ EOHQD/patch.js
@@ -1,10 +1,12 @@
 {
-  "dependencies": [
-    "nmlgc/base_tsa",
-    "nmlgc/base_tasofro",
-    "nmlgc/script_latin",
-  ],
-    "id": "EOHQD",
-    "title": "Embodiment of High Quality Devil",
-    "servers": ["https://raw.githubusercontent.com/Louchan3/thlouchan3/master/EOHQD"]
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"nmlgc/base_tasofro",
+		"nmlgc/script_latin"
+	],
+	"id": "EOHQD",
+	"servers": [
+		"https://raw.githubusercontent.com/Louchan3/thlouchan3/master/EOHQD/"
+	],
+	"title": "Embodiment of High Quality Devil"
 }
